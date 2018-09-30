686d607a494bd936e928d5c6d47e5f89229eab58
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-09-30 14:05:20 -0400

    created bird class

--- flappy_bird.py
+++ flappy_bird.py
@@ -14,6 +14,69 @@ ANIMATION_SPEED = 0.18
 WIN_WIDTH = 284 * 2
 WIN_HEIGHT = 512
 
+class Bird(pygame.sprite.Sprite):
+  # bird should be controlled by the player.
+   """Represents the bird controlled by the player.
+
+    The bird is the 'hero' of this game.  The player can make it climb
+    (ascend quickly), otherwise it sinks (descends more slowly).  It must
+    pass through the space in between pipes (for every pipe passed, one
+    point is scored); if it crashes into a pipe, the game ends.
+
+    Attributes:
+    x: The bird's X coordinate.
+    y: The bird's Y coordinate.
+    msec_to_climb: The number of milliseconds left to climb, where a
+        complete climb lasts Bird.CLIMB_DURATION milliseconds.
+
+    Constants:
+    WIDTH: The width, in pixels, of the bird's image.
+    HEIGHT: The height, in pixels, of the bird's image.
+    SINK_SPEED: With which speed, in pixels per millisecond, the bird
+        descends in one second while not climbing.
+    CLIMB_SPEED: With which speed, in pixels per millisecond, the bird
+        ascends in one second while climbing, on average.  See also the
+        Bird.update docstring.
+    CLIMB_DURATION: The number of milliseconds it takes the bird to
+        execute a complete climb.
+    """
+    WIDTH = height = 32
+    SINK_SPEED = 0.18
+    CLIMB_SPEED = 0.32
+    CLIMB_DURATION = 333.3
+
+    def __init__(self, x, y, msec_to_climb, images):
+      """Initialise a new Bird instance.
+
+        Arguments:
+        x: The bird's initial X coordinate.
+        y: The bird's initial Y coordinate.
+        msec_to_climb: The number of milliseconds left to climb, where a
+            complete climb lasts Bird.CLIMB_DURATION milliseconds.  Use
+            this if you want the bird to make a (small?) climb at the
+            very beginning of the game.
+        images: A tuple containing the images used by this bird.  It
+            must contain the following images, in the following order:
+                0. image of the bird with its wing pointing upward
+                1. image of the bird with its wing pointing downward
+        """
+      super(Bird,self).__init__()
+      self.x, self.y = x,y
+      self.msec_to_climb = msec_to_climb
+      self.img_wingup, self.img_wingdown = images
+      self._mask_wingup = pygame.mask.from_surface(self._img_wingdown)
+      self._mask_wingdown = pygame.mask.from_surface(self._img_wingdown)
+      
+      def mask(self):
+        if pygame.time.get_ticks() % 500 >= 250:
+          return self._mask_wingup
+        else:
+          return self._mask_wingdown
+
+        """Get a bitmask for use in collision detection.
+
+        The bitmask excludes all pixels in self.image with a
+        transparency greater than 127."""
 
 
 def load_images():
